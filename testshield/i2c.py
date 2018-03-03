36e7203e415f8591ece157f0c257df9a328d1834
Glenn Ruben Bakke <glennbakke@gmail.com>
2018-03-03 23:07:06 +0100

    started on testshield i2c test.

---
+++ testshield/i2c.py
@@ -0,0 +1,17 @@
+import os
+from machine import I2C, Pin
+
+mch = os.uname().machine
+if 'PCA10028' in mch:
+    pass
+elif 'PCA10040' in mch:
+    scl = Pin.board.PA24
+    sda = Pin.board.PA23
+elif 'PCA10056' in mch:
+    pass
+else:
+    raise Exception('Board not supported!')
+
+i = I2C(0, scl, sda)
+res = i.scan()
+print(res)
